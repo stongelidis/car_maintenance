dd9c3f2fedfaf28d39fee5ad1865c247994fb6be
Stephen Tongelidis <30709308+stongelidis@users.noreply.github.com>
2020-06-02 21:58:14 -0400

    refactor: remove environment variables from repository

--- setup.sh
+++
@@ -1,4 +0,0 @@
-export DATABASE_URL=postgresql://postgres:mysecretpassword@localhost/car_maintenance
-export AUTH0_CLIENT_ID='4aQ3qko6uXjy3XBucs63uGVfgpzcsc2Q'
-export AUTH0_DOMAIN='dev-ukvu27nm.auth0.com'
-export API_AUDIENCE='car_maintenance'
